7bf9b99ffbb5d8bbc13523d4208d796536e24850
polymath.nong <polymath.nong@gmail.com>
2026-09-12 15:37:25 -0500

    lowest common ancestor practice on a binary tree

---
+++ src/LCCI/treesAndGraphs/8_first_common_ancestor.py
@@ -0,0 +1,25 @@
+"""
+First Common Ancestor: Design an algorithm and write code to find the first common ancestor of two nodes in a binary tree. 
+Avoid storing additional nodes in a data structure. NOTE: This is not necessarily a binary search tree.
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p , q)
+        right = self.lowestCommonAncestor(root.right, p , q)
+
+        if left and right:
+            return root
+        else:
+            return left or right
